3aef7707131dc2d78bc355c6aa5d839d19baae93
00void00 <63497724+00void00@users.noreply.github.com>
2020-06-10 22:02:06 +0530

    match replaced by test
    as it didn't checked the format

--- index.js
+++ index.js
@@ -12,21 +12,28 @@ function validateNAME() {
 function validatePHONE() {
   var pno = document.getElementById("phone").value;
   var syn = /^\d(3)-\d(3)-\d(4)$/;
-  if (syn.match(pno)) {
+  if (syn.test(pno)) 
+  {
     return true;
-  } else {
+  }
+  else 
+  {
     alert("invalid phone number format");
     return false;
   }
 }
 
-function validatePASS() {
+function validatePASS() 
+{
   var psw1 = document.getElementById("password").value;
   var psw2 = document.getElementById("re-password").value;
-  if (psw1 == psw2) {
+  if (psw1 == psw2) 
+  {
     //returns ture if passwords entered by user matches
     return true;
-  } else {
+  } 
+  else 
+  {
     alert("password entered does not match");
     return false;
   }
